ee78618f3877b1856a913c689d4996ba940950e9
LIZ-ZZJ <zzijund@163.com>
2023-09-05 09:01:08 +0800

    暂存关于各项cmake的额外修改

--- main.cpp
+++ main.cpp
@@ -1,12 +1,14 @@
 #include <srm/nn.h>
 
 #include <iostream>
-
-#include "srm/video.h"
+#include <opencv2/opencv.hpp>
 
 int main() {
-  auto it = srm::nn::CreateYolo("coreml");
+  auto it = srm::nn::CreateYolo("tensorrt");
   std::cout << it << std::endl;
-  it->Initialize("test", 36, 4);
+  it->Initialize("armor.onnx", 36, 4);
+  auto img = cv::imread("test.jpg");
+  auto objs = it->Run(img);
+  for (auto &x : objs) std::cout << x.x1 << " " << x.y1 << " " << x.x2 << " " << x.y2 << std::endl;
   return 0;
 }
