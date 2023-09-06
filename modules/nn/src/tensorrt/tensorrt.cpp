4e53929ed2912f086ae124bec665251f79f48fd6
XY_cpp <xy_cpp@icloud.com>
2023-09-07 01:39:22 +0800

    合并、更名tensorrt

--- modules/nn/src/tensorrt/tensorrt.cpp
+++ modules/nn/src/tensorrt/tensorrt.cpp
@@ -1,4 +1,4 @@
-#include "yolo-network.h"
+#include "tensorrt.h"
 
 #include <NvOnnxParser.h>
 #include <common.h>
@@ -6,6 +6,39 @@
 #include <filesystem>
 
 namespace srm::nn {
+
+class TensorRT final : public Yolo {
+ public:
+  TensorRT() = default;
+  ~TensorRT() override;
+  bool Initialize(std::string REF_IN model_file, int num_classes, int num_points) override;
+  std::vector<Objects> Run(cv::Mat image) override;
+
+ private:
+  inline static auto registry_ = factory::RegistrySub<Yolo, TensorRT>("tensorrt");
+
+  int batches_{}, channels_{};
+
+  std::string model_file_path_, model_cache_path_;
+
+  nvinfer1::IRuntime *runtime_{};
+  nvinfer1::ICudaEngine *engine_{};
+  nvinfer1::IExecutionContext *execution_context_{};
+  cudaStream_t stream_{};
+  TRTLogger logger_;
+
+  int input_numel_ = 0;
+  float *input_data_host_{};
+  float *input_data_device_{};
+
+  int output_numel_ = 0;
+  float *output_data_host_{};
+  float *output_data_device_{};
+
+  void BuildEngineFromONNX();
+  void BuildEngineFromCache();
+};
+
 TensoRT::~TensorRT() {
   delete execution_context_;
   delete engine_;
